32562a6bb3d22ff127087496773b471f8f228906
mohamdzezo <35547874+mohamdzezo@users.noreply.github.com>
2019-03-14 22:10:24 +0200

    Create 10935 - Throwing cards away I.cpp

---
+++ UVa/10935 - Throwing cards away I.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+ 
+int main()
+{
+ 
+    int n ;
+    while(cin>>n,n!=0){
+ 
+        queue <int> ex;
+        for(int i=1;i<=n;i++){
+            ex.push(i);
+        }
+ 
+        cout<<"Discarded cards:";
+        while (ex.size()>1){
+                int move;
+                cout<<" "<<ex.front(); if(ex.size()!=2) cout<<",";
+                ex.pop();
+                move=ex.front();
+                ex.pop();
+                ex.push(move);
+        }
+        cout<<"\nRemaining card: "<<ex.front()<<"\n";
+    }
+ 
+ 
+    return 0;
+}
