9ddf9df5fd9ba12d3abfbfd808f5c3b3e6b3abf0
mohamdzezo <35547874+mohamdzezo@users.noreply.github.com>
2019-06-27 15:31:10 +0200

    Create 96 - A. Football.py

---
+++ Codeforces/Div.2 A/96 - A. Football.py
@@ -0,0 +1,11 @@
+"""***********************************************************
+# by           : mohamd abda alazez hashim                   #
+# e-mail       : mohamdmido2011@gmail.com                    #
+# problem      : 96 - A. Football                            #
+# Time to solve: 2 min                                       #
+***********************************************************"""
+s=input()
+if '0'*7 in s or '1'*7 in s:
+    print("YES")
+else:
+    print("NO")
