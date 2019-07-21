d28a937fb1623860019e28c68b5466ba70889ccf
mohamdzezo <35547874+mohamdzezo@users.noreply.github.com>
2019-07-21 03:31:02 +0200

    Create 664A - Complicated GCD.cpp

---
+++ Codeforces/Div.2 A/664A - Complicated GCD.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+//reading time : 2
+//thinking time: 1
+//coding time  : 1
+using namespace std;
+ 
+ 
+int main()
+{
+    string num1,num2;
+    cin>>num1>>num2;
+ 
+    if(num1==num2)
+        cout<<num1;
+    else
+        cout<<1;
+}
