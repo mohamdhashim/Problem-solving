1b2c7fb0fb10616e6a62eec7a0af9e2441cd6bc6
mohamdzezo <35547874+mohamdzezo@users.noreply.github.com>
2020-03-30 20:20:23 +0200

    Create Input.cpp

---
+++ Helpful codes/Input.cpp
@@ -0,0 +1,8 @@
+//gets(to take input with line);
+//get char to ignore n in input in gets();
+//sscanf(takestring to input);
+int n;
+   cin>>n;
+getchar();
+char s[5];
+ while(gets(s)&&s[0]!='\0'){ sscanf(s,"%d",i,...); ....}
